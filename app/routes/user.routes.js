9534fd9049b9aaa03ed7774ccb749a633b29f8f7
TomasBraden226 <sharplight226@gmail.com>
2023-03-24 12:35:58 +0900

    Start project

---
+++ app/routes/user.routes.js
@@ -0,0 +1,41 @@
+const { authJwt } = require("../middlewares");
+const { verifySignUp } = require("../middlewares");
+const controller = require("../controllers/user.controller");
+
+module.exports = function (app) {
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
+    next();
+  });
+
+  //Get all users data
+  app.get(
+    "/api/users",
+    [authJwt.verifyToken, authJwt.isAdmin],
+    controller.allUsers
+  );
+
+  //Create user
+  app.post(
+    "/api/user/create",
+    [authJwt.verifyToken, authJwt.isAdmin],
+    [
+      verifySignUp.checkDuplicateUsernameOrEmail,
+      verifySignUp.checkRolesExisted,
+    ],
+    controller.createUser
+  );
+
+  //Create user
+  app.post(
+    "/api/user/update",
+    [authJwt.verifyToken, authJwt.isAdmin],
+    controller.updateUser
+  );
+
+  app.delete(
+    "/api/user/delete",
+    [authJwt.verifyToken, authJwt.isAdmin],
+    controller.deleteUser
+  );
+};
